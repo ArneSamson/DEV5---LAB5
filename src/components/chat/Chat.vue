bf19f4cf6e6ba66a0ab3227bc5884d0df00d890d
ArneSamson <arnesamson007@gmail.com>
2023-11-19 10:52:16 +0100

    feat: addbase files from Vue claass

---
+++ src/components/chat/Chat.vue
@@ -0,0 +1,32 @@
+<template>
+    <div>
+        <ul >
+            <li v-for="m in allMessages.data">{{ m }}</li>
+        </ul>
+    </div>
+
+    <div>
+        <input v-model="message" type="text" placeholder="Type your mesage here" @keyup.input.enter="sendMessage">
+        <button @click="sendMessage">Send</button>
+    </div>
+</template>
+
+<script setup>
+    import { ref, reactive, onMounted} from 'vue';
+
+    let message = ref("");
+    let allMessages = reactive({
+        data: ["hey", "hello", "hi", "sup"],
+    });
+
+    function sendMessage() {
+        if(message.value !== ""){
+            allMessages.data.push(message.value);
+        }
+    }
+
+</script>
+
+<style scoped>
+
+</style>
